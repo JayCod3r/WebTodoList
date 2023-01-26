50947e1beab70b9f02c3c71a45cb744fffe7db0d
guro1881 <casiopains@gmail.com>
2023-01-26 18:17:13 +1100

    Added more functions
    Added more functions and added descriptive comments to functions

--- webTodoList.js
+++ webTodoList.js
@@ -1,5 +1,6 @@
 console.log("Loading localDB");
 var toDos = new LDB.Collection("ToDos");
+//Adds Todo object to local database
 function CreateToDo(name, filler) {
     var todo = {
         name: name,
@@ -8,22 +9,44 @@ function CreateToDo(name, filler) {
         id: GetLastId() + 1
     }
     toDos.save(todo, function (_items) {
-
+        console.log("ToDo Added @ id :" + _items[0].id);
     });
 }
-
+//Helper function. Gets id of last todo item
 function GetLastId() {
     if (toDos.items.length == 0) {
         return 0;
     }
     return toDos.items[toDos.items.length - 1].id;
 }
-
+//Drops all records from Todos
 function PurgeDatabase() {
     toDos.drop();
 }
-
-
+//Edits todo @ Id
+function EditToDo(lookUpId, name, filler) {
+    toDos.find({ id: lookUpId }, function (results) {
+        if (results[0]) {
+            results[0].name = name;
+            results[0].filler = filler;
+            results[0].save();
+        }
+    });
+}
+//Returns array of all todo items in database. For you jacobius!
+function GetAllToDos() {
+    return toDos.items;
+}
+//Toggles todo done state @ Id
+function ToggleDone(lookUpId) {
+    toDos.find({ id: lookUpId }, function (results) {
+        if (results[0]) {
+            results[0].done = !results[0].done;
+            results[0].save();
+        }
+    });
+}
+//Drops record @ Id
 function DeleteToDo(lookUpId) {
     toDos.find({ id: lookUpId }, function (items) {
         for (var i in items) {
